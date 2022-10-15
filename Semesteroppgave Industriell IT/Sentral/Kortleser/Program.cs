e33cd77828ac537f9db54672eb708f25f33a479b
William Antonesen <590413@stud.hvl.no>
2022-10-15 14:24:44 +0200

    Oppdatert Form1 -> Kortleser_GUI

--- Semesteroppgave Industriell IT/Sentral/Kortleser/Program.cs
+++ Semesteroppgave Industriell IT/Sentral/Kortleser/Program.cs
@@ -16,7 +16,7 @@ namespace Kortleser
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Kortleser_GUI());
         }
     }
 }
